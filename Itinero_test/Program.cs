f2ad974a7db288cd27c1ab8eced638191da663ed
Mateusz Rogalski <maroga@netwise.pl>
2020-05-21 18:23:58 +0200

    Add project files.

---
+++ Itinero_test/Program.cs
@@ -0,0 +1,80 @@
+﻿using Itinero;
+using Itinero.IO.Osm;
+using Itinero.LocalGeo;
+using Itinero.Osm.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itinero_test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//ProcessDbToGetOnlyCarRoutes();
+			RouterDb routerDb = null;
+			using (var stream = new FileInfo(@"C:\GIT\private\map\poland.routerdb").OpenRead())
+			{
+				routerDb = RouterDb.Deserialize(stream);
+			}
+			var router = new Router(routerDb);
+
+			//get a profile
+			var profile = Vehicle.Car.Fastest(); //the default OSM car profile
+
+
+			routerDb.AddContracted(profile);
+
+			Coordinate from = new Coordinate(52.255135f, 20.982435f);
+			Coordinate to = new Coordinate(53.111618f, 20.383173f);
+
+			//create a routerpoint from a location
+			//snaps the given location to the nearest routable edge
+			var start = router.TryResolve(profile, from, 200).Value;
+			var end = router.TryResolve(profile, to, 200).Value;
+
+			List<RouterPoint> points = new List<RouterPoint>()
+			{
+				router.TryResolve(profile, from, 200).Value,
+				router.TryResolve(profile, to, 200).Value
+			};
+
+
+
+			//calculate a route
+			//var route = router.Calculate(profile, start, end);
+			var stopwatch = new System.Diagnostics.Stopwatch();
+			stopwatch.Start();
+			var route = router.TryCalculate(profile, points.ToArray());
+			stopwatch.Stop();
+			Console.WriteLine($"Elapsed: {stopwatch.Elapsed.Minutes} min {stopwatch.Elapsed.Seconds} sec");
+			if (!route.IsError)
+			{
+				using (var writer = new StreamWriter("route.geojson"))
+				{
+					route.Value.WriteGeoJson(writer);
+				}
+			}
+		}
+
+		private static void ProcessDbToGetOnlyCarRoutes()
+		{
+			var routerDb = new RouterDb();
+			using (var stream = new FileInfo(@"C:\GIT\private\map\poland-latest.osm.pbf").OpenRead())
+			{
+				// create the network for cars.
+				routerDb.LoadOsmData(stream, Vehicle.Car);
+			}
+
+			// write the routerDb to disk
+			using (var stream = new FileInfo(@"C:\GIT\private\map\poland.routerdb").Open(FileMode.Create))
+			{
+				routerDb.Serialize(stream);
+			}
+		}
+	}
+}
---
+++ Mapsui_mytest/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Mapsui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BruTile.Predefined;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using Itinero;
+using System.IO;
+using Itinero.IO.Osm;
+using Itinero.Osm.Vehicles;
+
+namespace Mapsui_mytest
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			MyMapControl.Map.Layers.Add(new TileLayer(KnownTileSources.Create()));
+
+			var eitiCoordinates = new Point(21.011769094336856, 52.2190040831747);
+
+			var sphericalMarcatorCoordinate = SphericalMercator.FromLonLat(eitiCoordinates.X, eitiCoordinates.Y);
+
+			MyMapControl.Map.Home = n => n.NavigateTo(sphericalMarcatorCoordinate, MyMapControl.Map.Resolutions[17]);
+
+
+
+		}
+	}
+}

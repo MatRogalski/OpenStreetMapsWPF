35ef3c979e1cbdc05b9dc6a278078c2c24a5891c
Mateusz Rogalski <maroga@netwise.pl>
2020-05-22 20:43:33 +0200

    adding polyline to map

--- Mapsui_mytest/MainWindow.xaml.cs
+++ Mapsui_mytest/MainWindow.xaml.cs
@@ -22,6 +22,11 @@ using Itinero.IO.Osm;
 using Itinero.Osm.Vehicles;
 using Mapsui_mytest.Model;
 using Newtonsoft.Json;
+using Itinero.LocalGeo;
+using Microsoft.Maps.MapControl.WPF;
+using System.Globalization;
+using MapControl;
+using ViewModel;
 
 namespace Mapsui_mytest
 {
@@ -33,29 +38,23 @@ namespace Mapsui_mytest
 		public MainWindow()
 		{
 			InitializeComponent();
-			MyMapControl.Map.Layers.Add(new TileLayer(KnownTileSources.Create()));
 
-			var eitiCoordinates = new Point(21.011769094336856, 52.2190040831747);
+			ViewModel.Polyline polyline = GetMapControlPolyLine();
 
-			var sphericalMarcatorCoordinate = SphericalMercator.FromLonLat(eitiCoordinates.X, eitiCoordinates.Y);
-
-			MyMapControl.Map.Home = n => n.NavigateTo(sphericalMarcatorCoordinate, MyMapControl.Map.Resolutions[17]);
-
-
-
-			Polyline polyline = GetPolyLine();
-
-			//dd.
-			//MyMapControl.Map.Layers.Add(new Layer())
-
+			AddPolylineToMap(polyline);
 		}
 
-		private static Polyline GetPolyLine()
+		private void AddPolylineToMap(ViewModel.Polyline polyline)
 		{
-			var text = File.ReadAllText(@"C:\GIT\private\Mapsui_mytest\Itinero_test\bin\x64\Debug\route.geojson");
-			var parsed = JsonConvert.DeserializeObject<GeoJsonModel>(text);
-			var polyline = new Polyline();
-			foreach (var feature in parsed.features)
+			(this.DataContext as MapViewModel).Polylines.Add(polyline);
+		}
+
+		private static System.Windows.Shapes.Polyline GetPolyline()
+		{
+			string text = File.ReadAllText(@"C:\GIT\private\Mapsui_mytest\Itinero_test\bin\x64\Debug\route.geojson");
+			GeoJsonModel parsed = JsonConvert.DeserializeObject<GeoJsonModel>(text);
+			var polyline = new System.Windows.Shapes.Polyline();
+			foreach (Feature feature in parsed.features)
 			{
 				var startingCoordinates = feature.geometry.coordinates[0] as Newtonsoft.Json.Linq.JArray;
 				//
@@ -70,6 +69,180 @@ namespace Mapsui_mytest
 			}
 
 			return polyline;
+		}
+
+		private static ViewModel.Polyline GetMapControlPolyLine()
+		{
+			string text = File.ReadAllText(@"C:\GIT\private\Mapsui_mytest\Itinero_test\bin\x64\Debug\route.geojson");
+			GeoJsonModel parsed = JsonConvert.DeserializeObject<GeoJsonModel>(text);
+			var polyline = new ViewModel.Polyline();
+			polyline.Locations = new MapControl.LocationCollection();
+			foreach (Feature feature in parsed.features)
+			{
+				if (feature.geometry.coordinates[0] is Newtonsoft.Json.Linq.JArray startingCoordinates)
+				{
+					polyline.Locations.Add(new MapControl.Location((float)startingCoordinates.Last(), (float)startingCoordinates.First()));
+				}
+
+				if (feature.geometry.coordinates[1] is Newtonsoft.Json.Linq.JArray endingCoordinates)
+				{
+					polyline.Locations.Add(new MapControl.Location((float)endingCoordinates.Last(), (float)endingCoordinates.First()));
+				}
+
+				if (feature.geometry.coordinates[0] is double longitude && feature.geometry.coordinates[1] is double latitude)
+				{
+					polyline.Locations.Add(new MapControl.Location(latitude, longitude));
+				}
+			}
+
+			return polyline;
+		}
+
+
+		private static Microsoft.Maps.MapControl.WPF.LocationCollection GetListOfLocations()
+		{
+			string text = File.ReadAllText(@"C:\GIT\private\Mapsui_mytest\Itinero_test\bin\x64\Debug\route.geojson");
+			GeoJsonModel parsed = JsonConvert.DeserializeObject<GeoJsonModel>(text);
+			var locs = new Microsoft.Maps.MapControl.WPF.LocationCollection();
+			
+			foreach (Feature feature in parsed.features)
+			{ 
+				if(feature.geometry.coordinates[0] is Newtonsoft.Json.Linq.JArray startingCoordinates )
+				{
+					//
+					locs.Add(new Microsoft.Maps.MapControl.WPF.Location((float)startingCoordinates.Last(), (float)startingCoordinates.First()));
+				}
+
+				///var startingPoint = new Point((double)startingCoordinates.Last(), (double)startingCoordinates.First());
+
+				if(feature.geometry.coordinates[1] is Newtonsoft.Json.Linq.JArray endingCoordinates )
+				{
+					locs.Add(new Microsoft.Maps.MapControl.WPF.Location((float)endingCoordinates.Last(), (float)endingCoordinates.First()));
+				}
+
+				if(feature.geometry.coordinates[0] is double longitude && feature.geometry.coordinates[1] is double latitude)
+				{
+					locs.Add(new Microsoft.Maps.MapControl.WPF.Location(latitude, longitude));
+				}
+				//
+				///var endingPoint = new Point((float)endingCoordinates.Last(), (float)endingCoordinates.First());
+
+				//polyline.Points.Add(startingPoint);
+				//polyline.Points.Add(endingPoint);
+			}
+
+			return locs;
+		}
+
+		private static Route GetRoute()
+		{
+			RouterDb routerDb = null;
+			using (FileStream stream = new FileInfo(@"C:\GIT\private\map\quebec.routerdb").OpenRead())
+			{
+				routerDb = RouterDb.Deserialize(stream);
+			}
+			var router = new Router(routerDb);
+
+			//get a profile
+			Itinero.Profiles.Profile profile = Vehicle.Car.Fastest(); //the default OSM car profile
+
+
+			//routerDb.AddContracted(profile); //dodawanie tego trwa bardzo długo, może się opłacać zrobić to przed wyznaczaniem wielu tras
+
+			var from = new Coordinate(45.532400f, -73.622885f);
+			var to = new Coordinate(45.545841f, -73.623474f);
+
+			//create a routerpoint from a location
+			//snaps the given location to the nearest routable edge
+			RouterPoint start = router.TryResolve(profile, from, 200).Value;
+			RouterPoint end = router.TryResolve(profile, to, 200).Value;
+
+			var points = new List<RouterPoint>()
+			{
+				router.TryResolve(profile, from, 200).Value,
+				router.TryResolve(profile, to, 200).Value
+			};
+
+
+
+			//calculate a route
+			//var route = router.Calculate(profile, start, end);
+			Result<Route> route = router.TryCalculate(profile, points.ToArray());
+			return route.Value;
+		}
+
+
+
+
+		private void MapMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ClickCount == 2)
+			{
+				//map.ZoomMap(e.GetPosition(map), Math.Floor(map.ZoomLevel + 1.5));
+				//map.ZoomToBounds(new BoundingBox(53, 7, 54, 9));
+				map.TargetCenter = map.ViewToLocation(e.GetPosition(map));
+			}
+		}
+
+		private void MapMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ClickCount == 2)
+			{
+				//map.ZoomMap(e.GetPosition(map), Math.Ceiling(map.ZoomLevel - 1.5));
+			}
+		}
+
+		private void MapMouseMove(object sender, MouseEventArgs e)
+		{
+			MapControl.Location location = map.ViewToLocation(e.GetPosition(map));
+			int latitude = (int)Math.Round(location.Latitude * 60000d);
+			int longitude = (int)Math.Round(MapControl.Location.NormalizeLongitude(location.Longitude) * 60000d);
+			char latHemisphere = 'N';
+			char lonHemisphere = 'E';
+
+			if (latitude < 0)
+			{
+				latitude = -latitude;
+				latHemisphere = 'S';
+			}
+
+			if (longitude < 0)
+			{
+				longitude = -longitude;
+				lonHemisphere = 'W';
+			}
+
+			mouseLocation.Text = string.Format(CultureInfo.InvariantCulture,
+				"{0}  {1:00} {2:00.000}\n{3} {4:000} {5:00.000}",
+				latHemisphere, latitude / 60000, (latitude % 60000) / 1000d,
+				lonHemisphere, longitude / 60000, (longitude % 60000) / 1000d);
+		}
+
+		private void MapMouseLeave(object sender, MouseEventArgs e)
+		{
+			mouseLocation.Text = string.Empty;
+		}
+
+		private void MapManipulationInertiaStarting(object sender, ManipulationInertiaStartingEventArgs e)
+		{
+			e.TranslationBehavior.DesiredDeceleration = 0.001;
+		}
+
+		private void MapItemTouchDown(object sender, TouchEventArgs e)
+		{
+			var mapItem = (MapItem)sender;
+			mapItem.IsSelected = !mapItem.IsSelected;
+			e.Handled = true;
+		}
+
+		private void SeamarksChecked(object sender, RoutedEventArgs e)
+		{
+			map.Children.Insert(map.Children.IndexOf(mapGraticule), ((MapViewModel)DataContext).MapLayers.SeamarksLayer);
+		}
+
+		private void SeamarksUnchecked(object sender, RoutedEventArgs e)
+		{
+			map.Children.Remove(((MapViewModel)DataContext).MapLayers.SeamarksLayer);
 		}
 	}
 }
---
+++ Mapsui_mytest/MapLayers.cs
@@ -0,0 +1,205 @@
+﻿using MapControl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+#if WINDOWS_UWP
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+#else
+using System.Windows;
+using System.Windows.Media;
+#endif
+
+namespace ViewModel
+{
+    public class MapLayers : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly Dictionary<string, UIElement> mapLayers = new Dictionary<string, UIElement>
+        {
+            {
+                "OpenStreetMap",
+                new MapTileLayer
+                {
+                    SourceName = "OpenStreetMap",
+                    Description = "© [OpenStreetMap Contributors](http://www.openstreetmap.org/copyright)",
+                    TileSource = new TileSource { UriFormat = "https://{c}.tile.openstreetmap.org/{z}/{x}/{y}.png" },
+                    MaxZoomLevel = 19
+                }
+            },
+            {
+                "OpenStreetMap German",
+                new MapTileLayer
+                {
+                    SourceName = "OpenStreetMap German",
+                    Description = "© [OpenStreetMap contributors](http://www.openstreetmap.org/copyright)",
+                    TileSource = new TileSource { UriFormat = "https://{c}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png" },
+                    MaxZoomLevel = 19
+                }
+            },
+            {
+                "Stamen Terrain",
+                new MapTileLayer
+                {
+                    SourceName = "Stamen Terrain",
+                    Description = "Map tiles by [Stamen Design](http://stamen.com/), under [CC BY 3.0](http://creativecommons.org/licenses/by/3.0)\nData by [OpenStreetMap](http://openstreetmap.org/), under [ODbL](http://www.openstreetmap.org/copyright)",
+                    TileSource = new TileSource { UriFormat = "http://tile.stamen.com/terrain/{z}/{x}/{y}.png" },
+                    MaxZoomLevel = 17
+                }
+            },
+            {
+                "Stamen Toner Light",
+                new MapTileLayer
+                {
+                    SourceName = "Stamen Toner Light",
+                    Description = "Map tiles by [Stamen Design](http://stamen.com/), under [CC BY 3.0](http://creativecommons.org/licenses/by/3.0)\nData by [OpenStreetMap](http://openstreetmap.org/), under [ODbL](http://www.openstreetmap.org/copyright)",
+                    TileSource = new TileSource { UriFormat = "http://tile.stamen.com/toner-lite/{z}/{x}/{y}.png" },
+                    MaxZoomLevel = 18
+                }
+            },
+            {
+                "Bing Maps Road",
+                new BingMapsTileLayer
+                {
+                    SourceName = "Bing Maps Road",
+                    Description = "© [Microsoft](http://www.bing.com/maps/)",
+                    Mode = BingMapsTileLayer.MapMode.Road
+                }
+            },
+            {
+                "Bing Maps Aerial",
+                new BingMapsTileLayer
+                {
+                    SourceName = "Bing Maps Aerial",
+                    Description = "© [Microsoft](http://www.bing.com/maps/)",
+                    Mode = BingMapsTileLayer.MapMode.Aerial,
+                    MaxZoomLevel = 21,
+                    MapForeground = new SolidColorBrush(Colors.White),
+                    MapBackground = new SolidColorBrush(Colors.Black)
+                }
+            },
+            {
+                "Bing Maps Aerial with Labels",
+                new BingMapsTileLayer
+                {
+                    SourceName = "Bing Maps Hybrid",
+                    Description = "© [Microsoft](http://www.bing.com/maps/)",
+                    Mode = BingMapsTileLayer.MapMode.AerialWithLabels,
+                    MapForeground = new SolidColorBrush(Colors.White),
+                    MapBackground = new SolidColorBrush(Colors.Black)
+                }
+            },
+            {
+                "OpenStreetMap WMS",
+                new WmsImageLayer
+                {
+                    Description = "© [terrestris GmbH & Co. KG](http://ows.terrestris.de/)\nData © [OpenStreetMap contributors](http://www.openstreetmap.org/copyright)",
+                    ServiceUri = new Uri("http://ows.terrestris.de/osm/service")
+                }
+            },
+            {
+                "TopPlusOpen WMS",
+                new WmsImageLayer
+                {
+                    Description = "© [BKG](https://gdz.bkg.bund.de/index.php/default/webdienste/topplus-produkte/wms-topplusopen-mit-layer-fur-normalausgabe-und-druck-wms-topplus-open.html)",
+                    ServiceUri = new Uri("https://sgx.geodatenzentrum.de/wms_topplus_open")
+                }
+            },
+            {
+                "TopPlusOpen WMTS",
+                new WmtsTileLayer
+                {
+                    Description = "© [BKG](https://gdz.bkg.bund.de/index.php/default/webdienste/topplus-produkte/wmts-topplusopen-wmts-topplus-open.html)",
+                    CapabilitiesUri = new Uri("https://sgx.geodatenzentrum.de/wmts_topplus_open/1.0.0/WMTSCapabilities.xml"),
+                    SourceName = "TopPlusOpen"
+                }
+            },
+            {
+                "SevenCs ChartServer WMS",
+                new ChartServerLayer()
+            },
+            {
+                "Seamarks",
+                new MapTileLayer
+                {
+                    SourceName = "OpenSeaMap",
+                    TileSource = new TileSource { UriFormat = "http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png" },
+                    MinZoomLevel = 9,
+                    MaxZoomLevel = 18
+                }
+            },
+        };
+
+        private string currentMapLayerName = "OpenStreetMap";
+
+        public string CurrentMapLayerName
+        {
+            get { return currentMapLayerName; }
+            set
+            {
+                currentMapLayerName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentMapLayerName)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentMapLayer)));
+            }
+        }
+
+        public UIElement CurrentMapLayer
+        {
+            get { return mapLayers[currentMapLayerName]; }
+        }
+
+        public UIElement SeamarksLayer
+        {
+            get { return mapLayers["Seamarks"]; }
+        }
+
+        public List<string> MapLayerNames { get; } = new List<string>
+        {
+            "OpenStreetMap",
+            "OpenStreetMap German",
+            "Stamen Terrain",
+            "Stamen Toner Light",
+            "OpenStreetMap WMS",
+            "TopPlusOpen WMS",
+            "TopPlusOpen WMTS",
+            "SevenCs ChartServer WMS",
+        };
+
+        public MapLayers()
+        {
+            //BingMapsTileLayer.ApiKey = "...";
+
+            // Bing Maps TileLayers with tile URLs retrieved from the Imagery Metadata Service
+            // (see http://msdn.microsoft.com/en-us/library/ff701716.aspx).
+            // A Bing Maps API Key (see http://msdn.microsoft.com/en-us/library/ff428642.aspx) is required
+            // for using these layers and must be assigned to the static BingMapsTileLayer.ApiKey property.
+
+            if (!string.IsNullOrEmpty(BingMapsTileLayer.ApiKey))
+            {
+                MapLayerNames.Add("Bing Maps Road");
+                MapLayerNames.Add("Bing Maps Aerial");
+                MapLayerNames.Add("Bing Maps Aerial with Labels");
+            }
+        }
+    }
+
+    public class ChartServerLayer : WmsImageLayer
+    {
+        public ChartServerLayer()
+        {
+            Description = "© [SevenCs GmbH](http://www.sevencs.com)";
+            ServiceUri = new Uri("https://wms.sevencs.com:9090");
+            Layers = "ENC";
+            MaxBoundingBoxWidth = 360;
+        }
+
+        protected override string GetImageUri()
+        {
+            return base.GetImageUri()
+                .Replace("&CRS=AUTO2:97001,", "&CRS=AUTO2:7CS01,")
+                .Replace("&CRS=AUTO2:97002,", "&CRS=AUTO2:7CS02,");
+        }
+    }
+}

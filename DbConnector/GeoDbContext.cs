3b6e99dfdcd7de6c4c93d7963306a1d25a302537
Mateusz Rogalski <maroga@netwise.pl>
2020-05-23 15:59:11 +0200

    cleaning solution

--- DbConnector/GeoDbContext.cs
+++ DbConnector/GeoDbContext.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Configuration;
-//using System.Data.Entity;
 
 namespace DbConnector
 {
--- Utilities/RandomRecordsPicker/Program.cs
+++ Utilities/RandomRecordsPicker/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Linq;
 
-namespace Utilities
+namespace RandomRecordsPicker
 {
 	class Program
 	{

3010389935904c2b5af818c9e06a70cf2e947174
Mateusz Rogalski <maroga@netwise.pl>
2020-05-23 18:19:13 +0200

    added shared project with view model and started making bare bones routing algorithm

---
+++ Router/Router.cs
@@ -0,0 +1,24 @@
+﻿using MapControl;
+using System;
+using ViewModel;
+
+namespace Router
+{
+	public class Router
+	{
+		public void GetRouteWithoutAgregatedPoints(Location startingPoint, Location endingPoint, double additionalDistance, double additionalTime)
+		{
+			 
+		}
+
+
+		protected Polyline GetRouteBetweenTwoPoints(Location startingPoint, Location endingPoint)
+		{
+			//TODO: needs implementation
+			var result = new Polyline();
+			result.Locations = new LocationCollection();
+
+			return result;
+		}
+	}
+}

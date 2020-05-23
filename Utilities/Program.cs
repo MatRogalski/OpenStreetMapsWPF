b156be421bde222e63b81563c205d0cf40ed5795
Mateusz Rogalski <maroga@netwise.pl>
2020-05-23 13:31:41 +0200

    added method to get n random elements from file

---
+++ Utilities/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Utilities
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int numberOfResults = 100;
+			GetNRecordsFromFile(numberOfResults, @"C:\GIT\private\map\openaddr-collected-europe\pl\mazowieckie.csv", @$"C:\GIT\private\map\mazowieckie_{numberOfResults}.csv");
+		}
+
+		private static void GetNRecordsFromFile(int numberOfResults, string sourcePath, string destinationPath)
+		{
+			var lines = File.ReadAllLines(sourcePath).ToList();
+
+			lines.RemoveAt(0);
+
+			var toSave = lines.OrderBy(x => Guid.NewGuid()).Take(numberOfResults).ToList();
+
+			string directory = Path.GetDirectoryName(destinationPath);
+			if (!string.IsNullOrWhiteSpace(directory) && !Directory.Exists(directory))
+			{
+				Directory.CreateDirectory(directory);
+			}
+
+			using (var writer = new StreamWriter(destinationPath))
+			{
+				foreach (string line in toSave)
+				{
+					writer.WriteLine(line);
+				}
+			}
+		}
+	}
+}

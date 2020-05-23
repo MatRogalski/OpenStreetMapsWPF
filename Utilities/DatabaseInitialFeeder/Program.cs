53f9b6fd87f8818bfd7c42570ec7a74534571693
Mateusz Rogalski <maroga@netwise.pl>
2020-05-23 17:55:33 +0200

    added app to insert data in db initially

---
+++ Utilities/DatabaseInitialFeeder/Program.cs
@@ -0,0 +1,54 @@
+﻿using DbConnector.Repositories;
+using DbModel;
+using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DatabaseInitialFeeder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			InsertDataFromFileToDb(@$"C:\GIT\private\map\mazowieckie_100.csv");
+
+		}
+
+		private static void InsertDataFromFileToDb(string fileName)
+		{
+			string[] lines = File.ReadAllLines(fileName);
+			var repo = new LocalizationPointRepository();
+
+			foreach (string line in lines)
+			{
+				string[] splitted = line.Split(",", StringSplitOptions.None);
+
+				if (splitted.Count() >= 9
+					&& double.TryParse(splitted[0], out double longitude)
+					&& double.TryParse(splitted[1], out double latitude))
+				{
+
+
+					var localizationPoint = new LocalizationPoint()
+					{
+						Coordinate = new Point(longitude, latitude) { SRID = 4326 }, //this refers to EPSG 4326
+						Number = splitted[2],
+						Street = splitted[3],
+						City = splitted[5],
+						District = splitted[6],
+						Region = splitted[7],
+						PostalCode = splitted[8],
+						StaticScore = 0,
+						InnerDistance = 0,
+						InnerTime = 0
+					};
+
+					repo.Add(localizationPoint);
+				}
+			}
+			repo.SaveChanges();
+		}
+	}
+}

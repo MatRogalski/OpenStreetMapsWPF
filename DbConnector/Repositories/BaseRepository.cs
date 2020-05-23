45c1214e54ce2ad60f2c6a4a0ed6437f50d450cb
Mateusz Rogalski <maroga@netwise.pl>
2020-05-23 16:24:48 +0200

    added repository for db connector

---
+++ DbConnector/Repositories/BaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DbConnector.Repositories
+{
+	public abstract class BaseRepository <TEntity> where TEntity : class
+	{
+        protected readonly GeoDbContext context;
+        protected readonly DbSet<TEntity> dataSet;
+
+        public BaseRepository()
+        {
+            this.context = new GeoDbContext();
+            this.dataSet = this.context.Set<TEntity>();
+        }
+
+        public virtual TEntity Add(TEntity entity)
+        {
+            return this.dataSet.Add(entity).Entity;
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            this.dataSet.Remove(entity);
+        }
+
+        //check this out
+        public virtual void Edit(TEntity entity)
+        {
+            this.context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this.dataSet.Where(predicate);
+        }
+
+        public virtual IQueryable<TEntity> GetAll()
+        {
+            return this.context.Set<TEntity>();
+        }
+
+    }
+}
---
+++ DbConnector/Repositories/LocalizationPointRepository.cs
@@ -0,0 +1,21 @@
+﻿using DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DbConnector.Repositories
+{
+	public class LocalizationPointRepository : BaseRepository<LocalizationPoint>
+	{
+		public LocalizationPointRepository() : base()
+		{
+
+		}
+
+		public List<LocalizationPoint> GetByParentId(long parentLocalizationPointId)
+		{
+			return this.dataSet.Where(i => i.ParentPointId == parentLocalizationPointId).ToList();
+		}
+	}
+}

eb307afddf0d338e3320edc3297f997741ade0c9
Kamil Tuszyński <miltenwml@gmail.com>
2020-05-29 22:33:03 +0200

    fix for processing aggregated points

--- Router/Router.cs
+++ Router/Router.cs
@@ -12,18 +12,18 @@ namespace Router
 {
 	public class Router : BaseRouter
 	{
-		private RouteModel lastDynamicScoreCalculatedRoute;		
+		private RouteModel lastDynamicScoreCalculatedRoute;
 		private bool dynamicScoreNeedsToBeRecalculated;
 
 		private const double AREA_RATIO_THRESHOLD = 0.5;
 
-		
+
 		public Router(Position startingPoint, Position endingPoint, double additionalDistance, double additionalTime) : base(startingPoint, endingPoint, additionalDistance, additionalTime)
 		{
 			this.doesRouteMeetParameters = true;
 			this.dynamicScoreNeedsToBeRecalculated = true;
 		}
-		
+
 		protected override void ProcessAvailablePoints(List<LocalizationPointDto> availablePoints, double currentAdditionalDistance, double currentAdditionalTime, int? stepSize = null)
 		{
 			// TODO: think about not processsing multiple times points that are aggregated 
@@ -46,6 +46,10 @@ namespace Router
 				else
 				{
 					this.AddAggregatedPoint(biggestScorePoint, ref currentAdditionalDistance, ref currentAdditionalTime);
+					// TODO: jesli punkt zaagregowany jest rozbity na pomniejsze punkty to pozwala na dalsze przetwarzanie zwyklych punktow
+					// ale za to jest szansa ze wejdziemy w petle nieskoczona - gdyby zostaly tylko punkty zaagregowane
+					// dlatego moze to usunac
+					this.doesRouteMeetParameters = true;
 				}
 			}
 		}
@@ -60,12 +64,14 @@ namespace Router
 			this.doesRouteMeetParameters = this.DoesRouteMeetParameters(newRoute, currentAdditionalDistance, currentAdditionalTime);
 			if (this.doesRouteMeetParameters)
 			{
+				this.waypoints.Clear();
 				this.waypoints.AddRange(tempWaypoints);
 				this.dynamicScoreNeedsToBeRecalculated = this.DoesDynamicScoreNeedToBeRecalculated(this.lastDynamicScoreCalculatedRoute, newRoute, (Position)biggestScorePoint.Point.Coordinates);
 				this.UpdateResultRoute(newRoute, out currentAdditionalDistance, out currentAdditionalTime);
 			}
 			else
 			{
+				this.doesRouteMeetParameters = true;
 				this.ProcessAvailablePoints(innerPoints, currentAdditionalDistance, currentAdditionalTime, (int)biggestScorePoint.InnerDistance / 10);
 			}
 		}
@@ -119,7 +125,7 @@ namespace Router
 							return points;
 
 					Microsoft.SqlServer.Types.SqlGeography bufferSqlGeography = routeSqlGeography.STBuffer(bufferSize);
-					result = points.Where(i => (bool)bufferSqlGeography.STContains(i.Point.ToSqlGeography())).ToList();					
+					result = points.Where(i => (bool)bufferSqlGeography.STContains(i.Point.ToSqlGeography())).ToList();
 
 					// may not work because reference type - seems to work
 					this.lastDynamicScoreCalculatedRoute = this.resultRoute;
@@ -171,7 +177,7 @@ namespace Router
 			return areaRatio > AREA_RATIO_THRESHOLD;
 		}
 
-		
+
 
 	}
 }
